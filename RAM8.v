8d9f8defb583d40ece5a333b1bc69a3496b07636
ibeckermayer <ibeckermayer@gmail.com>
2018-12-24 17:26:55 -0800

    RAM8 created, now should test

---
+++ RAM8.v
@@ -0,0 +1,12 @@
+module RAM8(out, address, in, load, clk);
+   output[15:0] out;
+   input [15:0] in;
+   input [2:0]  address;
+   input        load, clk;
+   reg [15:0]   ram [7:0];      // 8-element array of 16-bit wide reg
+   always @(posedge clk) begin
+      if (load)
+        ram[address] <= in;
+      out <= ram[address];
+   end
+endmodule // RAM8
