4f032a28781efdb8c17bc3bcf56b592c10e13cc4
ibeckermayer <ibeckermayer@gmail.com>
2018-12-29 00:10:12 -0800

    made out a clocked register

--- RAM8.v
+++ RAM8.v
@@ -3,10 +3,13 @@ module RAM8(out, address, in, load, clk);
    input [15:0] in;
    input [2:0]  address;
    input        load, clk;
+
+   reg          out;            // out is a register
    reg [15:0]   ram [7:0];      // 8-element array of 16-bit wide reg
+
    always @(posedge clk) begin
       if (load)
         ram[address] <= in;
-      out <= ram[address];
+      out <= ram[address];      // clocked assignment
    end
 endmodule // RAM8
