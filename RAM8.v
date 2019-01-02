cbe63d7346c4cf4858161b0515e66aa4d70df214
ibeckermayer <ibeckermayer@gmail.com>
2019-01-01 16:29:41 -0800

    spacing

--- RAM8.v
+++ RAM8.v
@@ -1,12 +1,13 @@
-module RAM8(
-            input             clk,
-            input [2:0]       address,
-            input             load,
-            input [15:0]      in,
-            output reg [15:0] out
-            );
+module RAM8
+  (
+   input             clk,
+   input [2:0]       address,
+   input             load,
+   input [15:0]      in,
+   output reg [15:0] out
+   );
 
-   reg [15:0]                 ram [7:0]; // 8-element array of 16-bit wide reg
+   reg [15:0]        ram [7:0]; // 8-element array of 16-bit wide reg
 
    always @(posedge clk) begin
       if (load)
