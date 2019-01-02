a7b67d635860f69fffddf21b12364c7c302372a3
ibeckermayer <ibeckermayer@gmail.com>
2019-01-01 16:24:54 -0800

    changed to modern instantiation

--- RAM8.v
+++ RAM8.v
@@ -1,12 +1,11 @@
-module RAM8(out, address, in, load, clk);
-   output[15:0] out;
-   input [15:0] in;
-   input [2:0]  address;
-   input        load, clk;
-
-   reg          out;            // out is a register
-   reg [15:0]   ram [7:0];      // 8-element array of 16-bit wide reg
-
+module RAM8(
+            input             clk,
+            input [2:0]       address,
+            input             load,
+            input [15:0]      in,
+            output reg [15:0] out
+            );
+   reg [15:0]                 ram [7:0]; // 8-element array of 16-bit wide reg
    always @(posedge clk) begin
       if (load)
         ram[address] <= in;
