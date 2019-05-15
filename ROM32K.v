297ad7266fac876c65b0381989feb02f47b77198
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-15 16:57:47 -0700

    specified comment further

--- ROM32K.v
+++ ROM32K.v
@@ -4,8 +4,8 @@ module ROM32K
    output [15:0] out
    );
 
-   reg [15:0]    ram [32767: 0]; // 32k ram
+   reg [15:0]    ram [32767: 0]; // 32k ram at 16-bit address width
 
    assign out = ram[address];
-   
+
 endmodule // ROM32K
