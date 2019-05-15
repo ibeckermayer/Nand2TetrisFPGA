9b5b3fc7e897a5650dbc1e91a3e5b0a73c6a2c5c
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-14 23:10:39 -0700

    skeleton of a tb

---
+++ ALU_tb.v
@@ -0,0 +1,14 @@
+module ALU_tb();
+   reg [15:0]  x;
+   reg [15:0]  y;
+   reg 	 zx;
+   reg 	 nx;
+   reg 	 zy;
+   reg 	 ny;
+   reg 	 f;
+   reg 	 no;
+   wire [15:0] out;
+   wire 	 zr;
+   wire 	 ng;
+
+endmodule // ALU_tb
