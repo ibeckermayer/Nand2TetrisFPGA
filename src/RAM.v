89b6a34f0447b32cfd369c160c01a2b3c1ab0aec
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-18 14:35:46 -0700

    made RAM asynch. Machine language spec does not expect this, however it doesn't hurt and allows for potential preformance gains by updating the spec to account for this in the future

--- src/RAM.v
+++ src/RAM.v
@@ -4,7 +4,7 @@ module RAM #(parameter RAM_WIDTH = 8, parameter ADDR_WIDTH = 3) // RAM_WIDTH == 
     input [ADDR_WIDTH-1:0] address,
     input 		   load,
     input [15:0] 	   in,
-    output reg [15:0] 	   out
+    output [15:0] 	   out
     );
 
    reg [15:0] 		   ram [RAM_WIDTH-1:0]; // RAM_WIDTH-element array of 16-bit wide reg
@@ -12,6 +12,6 @@ module RAM #(parameter RAM_WIDTH = 8, parameter ADDR_WIDTH = 3) // RAM_WIDTH == 
    always @(posedge clk) begin
       if (load)
         ram[address] <= in;
-      out <= ram[address];      // clocked assignment
    end
+   assign out = ram[address];	// asynchronous RAM
 endmodule // RAM8
