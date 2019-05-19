6eb8f6f9afa4d52c813b5e84ad1a398c3c2fb343
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-18 17:12:36 -0700

    updated build script for RAM_tb

--- test/build.sh
+++ test/build.sh
@@ -3,6 +3,8 @@
 
 # build test vector files
 python3 gen_tv/gen_pc_tv.py
+python3 gen_tv/gen_ram_tv.py
 
 # build test benches
 iverilog -o bin/PC_test PC_tb.v ../src/PC.v
+iverilog -o bin/RAM_test RAM_tb.v ../src/RAM.v
