3ddd2278d88a75cb9ce74eee70325fb12d6acc4f
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-19 22:15:53 -0700

    updated build script to build ROM files

--- test/build.sh
+++ test/build.sh
@@ -4,7 +4,9 @@
 # build test vector files
 python3 gen_tv/gen_pc_tv.py
 python3 gen_tv/gen_ram_tv.py
+python3 gen_tv/gen_rom_tv.py
 
 # build test benches
 iverilog -o bin/PC_test PC_tb.v ../src/PC.v
 iverilog -o bin/RAM_test RAM_tb.v ../src/RAM.v
+iverilog -o bin/ROM_test ROM_tb.v ../src/ROM32K.v
