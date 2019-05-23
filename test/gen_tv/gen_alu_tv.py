6d389d25229bf87e45daa348c39f904b35007423
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-22 21:28:10 -0700

    moved functions into util

--- test/gen_tv/gen_alu_tv.py
+++ test/gen_tv/gen_alu_tv.py
@@ -10,8 +10,8 @@ For each combination of zx, nx, zy, ny, f, no listed on page 56 of the pdf of th
   - x = 0, y = 0
   - check N times with random integers in [-32768, 32767]
 '''
-from util import int_to_bin_str
-from random import randint
+from util import (int_to_bin_str, gen_rand_int, bitwise_not, bitwise_and,
+                  bitwise_or, zr, ng)
 
 OUTPUT_FILE = 'tvs/ALU.tv'  # expects to be run from directory above this
 N = 1000  # number of times each function will be checked with random inputs for x and y
@@ -37,58 +37,6 @@ functions = [
     '000000',  # f(x,y) = x&y
     '010101'  # f(x,y) = x|y
 ]
-
-
-def gen_rand_int() -> int:
-    return randint(-32768, 32767)
-
-
-def bitwise_not(val: int) -> str:
-    '''
-    returns bitwise negated str representation of 16 bit integer
-    '''
-    strval = int_to_bin_str(val, 16)
-    notval = ''
-    for dig in strval:
-        if dig == '0':
-            notval += '1'
-        else:
-            notval += '0'
-    return notval
-
-
-def bitwise_and(x: int, y: int) -> str:
-    strx = int_to_bin_str(x, 16)
-    stry = int_to_bin_str(y, 16)
-    andval = ''
-    for i in range(16):
-        if (strx[i] == '1' and stry[i] == '1'):
-            andval += '1'
-        else:
-            andval += '0'
-    return andval
-
-
-def bitwise_or(x: int, y: int) -> str:
-    strx = int_to_bin_str(x, 16)
-    stry = int_to_bin_str(y, 16)
-    orval = ''
-    for i in range(16):
-        if (strx[i] == '1' or stry[i] == '1'):
-            orval += '1'
-        else:
-            orval += '0'
-    return orval
-
-
-def zr(out_str: str) -> str:
-    if out_str == int_to_bin_str(0, 16):
-        return '1'
-    return '0'
-
-
-def ng(out_str: str) -> str:
-    return out_str[0]
 
 
 def ALU_logic_and_build_line(x: int, y: int, function: str) -> str:
--- test/gen_tv/util.py
+++ test/gen_tv/util.py
@@ -1,4 +1,5 @@
 from numpy import binary_repr
+from random import randint
 
 
 def int_to_bin_str(val: int, width: int) -> str:
@@ -12,3 +13,55 @@ def int_to_bin_str(val: int, width: int) -> str:
         return retval[1:]
     else:
         return retval
+
+
+def gen_rand_int() -> int:
+    return randint(-32768, 32767)
+
+
+def bitwise_not(val: int) -> str:
+    '''
+    returns bitwise negated str representation of 16 bit integer
+    '''
+    strval = int_to_bin_str(val, 16)
+    notval = ''
+    for dig in strval:
+        if dig == '0':
+            notval += '1'
+        else:
+            notval += '0'
+    return notval
+
+
+def bitwise_and(x: int, y: int) -> str:
+    strx = int_to_bin_str(x, 16)
+    stry = int_to_bin_str(y, 16)
+    andval = ''
+    for i in range(16):
+        if (strx[i] == '1' and stry[i] == '1'):
+            andval += '1'
+        else:
+            andval += '0'
+    return andval
+
+
+def bitwise_or(x: int, y: int) -> str:
+    strx = int_to_bin_str(x, 16)
+    stry = int_to_bin_str(y, 16)
+    orval = ''
+    for i in range(16):
+        if (strx[i] == '1' or stry[i] == '1'):
+            orval += '1'
+        else:
+            orval += '0'
+    return orval
+
+
+def zr(out_str: str) -> str:
+    if out_str == int_to_bin_str(0, 16):
+        return '1'
+    return '0'
+
+
+def ng(out_str: str) -> str:
+    return out_str[0]
