068446871156e46a5984629fcc86e88c2af15858
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-22 21:54:59 -0700

    starting CPU_tb

---
+++ test/gen_tv/gen_cpu_tv.py
@@ -0,0 +1,11 @@
+'''
+script for generating the testvector file for use in ALU_tb.v
+
+Format will be:
+(inM)(instruction)_(outM_exp)(writeM_exp)(addressM_exp)(pc_exp)_(A_exp)(D_exp)
+NOTE that clk and reset should be set internally in the testbench
+
+Algorithm:
+- begin with a reset to set PC to zero (NOTE: do this in the testbench and set up checker logic s.t. it doesn't check for proper outputs on reset)
+- 
+'''
