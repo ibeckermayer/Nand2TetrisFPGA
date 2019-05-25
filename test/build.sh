f57e28f24ef0712f7fd360bda49192848a44ea55
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-25 11:57:48 -0700

    all tv generators now making use of simulator classes

--- test/build.sh
+++ test/build.sh
@@ -2,13 +2,13 @@
 # Bash script to build tests
 
 # build test vector files
-# python3 gen_tv/gen_pc_tv.py
+python3 gen_tv/gen_pc_tv.py
 python3 gen_tv/gen_ram_tv.py
 python3 gen_tv/gen_rom_tv.py
 python3 gen_tv/gen_alu_tv.py
 
 # build test benches
-# iverilog -o bin/PC_test PC_tb.v ../src/PC.v
+iverilog -o bin/PC_test PC_tb.v ../src/PC.v
 iverilog -o bin/RAM_test RAM_tb.v ../src/RAM.v
 iverilog -o bin/ROM_test ROM_tb.v ../src/ROM32K.v
 iverilog -o bin/ALU_test ALU_tb.v ../src/ALU.v
--- test/gen_tv/gen_pc_tv.py
+++ test/gen_tv/gen_pc_tv.py
@@ -1,9 +1,6 @@
 '''
 script for generating the testvector file for use in PC_tb.v
 
-Format will be:
-(in_v[16])(reset)(load)(inc)_(out_v[16]) in binary format
-
 Algorithm:
 - start with a reset, expect a 0
 - count up to 1000 to check increment
@@ -13,79 +10,44 @@ Algorithm:
 - count up to 65535
 - count up to 1000 again to check rollover
 '''
-from util import int_to_bin_str
+from util import PCSimulator
 
 OUTPUT_FILE = 'tvs/PC.tv'  # expects to be run from directory above this
 
-
-def PC_logic(in_v: int, reset: int, load: int, inc: int,
-             previous_out_v: int) -> int:
-    '''
-    implements expected logic of PC module to generate test file
-    returns integer that represents output of PC module for this input logic
-    '''
-    if (reset):
-        return 0
-    elif (load):
-        return in_v
-    elif (inc):
-        return previous_out_v + 1
-    else:
-        return previous_out_v
-
-
-def build_line(in_v: int, reset: int, load: int, inc: int,
-               previous_out_v: int):
-    '''
-    builds a line for output to the tv file
-    '''
-    next_out = PC_logic(in_v, reset, load, inc, previous_out_v)
-    line = int_to_bin_str(in_v, 16) + int_to_bin_str(
-        reset, 1) + int_to_bin_str(load, 1) + int_to_bin_str(
-            inc, 1) + '_' + int_to_bin_str(next_out, 16) + '\n'
-    return (line, next_out)
-
+pcsim = PCSimulator()
 
 # initial values
 in_v = 1
 reset = 1  # start with a reset
 load = 1  # start with load 1, see that reset has precedence
 inc = 1  # start with inc 1, see that reset has precedence
-out_v = 0  # output should be 0 due to reset
 
 with open(OUTPUT_FILE, 'w') as f:
-    # start with a reset, expect a 0
-    line, out_v = build_line(in_v, reset, load, inc, out_v)
-    f.write(line)
+    f.write(pcsim.build_line(in_v, reset, load, inc))
 
     # count up to 1000 to check increment
     reset = 0
     load = 0
     for i in range(1000):
-        line, out_v = build_line(in_v, reset, load, inc, out_v)
-        f.write(line)
+        f.write(pcsim.build_line(in_v, reset, load, inc))
 
     # reset again to check reset at arbitrary count
     reset = 1
-    line, out_v = build_line(in_v, reset, load, inc, out_v)
-    f.write(line)
+    f.write(pcsim.build_line(in_v, reset, load, inc))
 
     # count up to 1000 again
     reset = 0
     load = 0
     for i in range(1000):
-        line, out_v = build_line(in_v, reset, load, inc, out_v)
-        f.write(line)
+        f.write(pcsim.build_line(in_v, reset, load, inc))
 
     # load 65000 to test loading from arbitrary count
     load = 1
     in_v = 65000
-    line, out_v = build_line(in_v, reset, load, inc, out_v)
-    f.write(line)
+    f.write(pcsim.build_line(in_v, reset, load, inc))
 
     # count up to 65535
     # count up to 1000 again to check rollover
     load = 0
     for i in range(1001 + 535):
-        line, out_v = build_line(in_v, reset, load, inc, out_v)
-        f.write(line)
+        f.write(pcsim.build_line(in_v, reset, load, inc))
