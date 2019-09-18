de2b2701641ec47f933173a09a3b97f668c07b33
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-09-18 21:07:25 +0200

    implemented CPU tv generator, placed some important notes in the comment

--- test/build.sh
+++ test/build.sh
@@ -6,6 +6,7 @@ python3 gen_tv/gen_pc_tv.py
 python3 gen_tv/gen_ram_tv.py
 python3 gen_tv/gen_rom_tv.py
 python3 gen_tv/gen_alu_tv.py
+python3 gen_tv/gen_cpu_tv.py
 
 # build test benches
 iverilog -o bin/PC_test PC_tb.v ../src/PC.v
--- test/gen_tv/gen_cpu_tv.py
+++ test/gen_tv/gen_cpu_tv.py
@@ -2,9 +2,66 @@
 script for generating the testvector file for use in ALU_tb.v
 
 Format will be:
-(inM)(instruction)(reset)_(outM_exp)(writeM_exp)(addressM_exp)(pc_exp)_(A_exp)(D_exp)
+{inM[WIDTH], instruction[WIDTH], reset}_{outM[WIDTH], writeM, addressM[WIDTH], pc[WIDTH]}
 NOTE that clk and reset should be set internally in the testbench
 
 Algorithm:
-- begin with a reset to set PC to zero (NOTE: do this in the testbench and set up checker logic s.t. it doesn't check for proper outputs on reset)
+  - Follow the alu approach of blasting this with random numbers and seeing if both the python
+    model and the verilog model agree. This begs the question what's testing what? We're also
+    now running in to an unforseen complication wherein we can only load A-instructions that also
+    happen to have values that have a c1-c6 that would be a valid ALU function. This is because
+    of how the ALU logic is hardcoded in the python simulation. The verilog in ALU.v is a high
+    enough level of abstraction that I'm not sure what it does on an invalid instruction. It would
+    be interesting to check this model out in that Xilinx tool that allows you to see the block/wiring
+    diagram and/or a signal-probe simulation, since that might give you more insight into how the high 
+    level verilog is interpreted.
+    
+    This all makes me consider whether this is moment where formal verification might come into
+    play -- I've heard that it's used effectively for digital design. In some sense it would also
+    be a test of whether one's formal verification problem was set up correctly. I imagine that it
+    might be better because formal verification might make it easier to specify and test all the possible
+    edge cases. Perhaps I'm being lazy, but trying to do that using my python simulations for a machine as 
+    complex as the CPU is seems extremely daunting.
 '''
+from simulators.cpu import CPUSimulator
+from simulators.alu import ALUSimulator, UnkownALUFunction
+from random import randint
+
+OUTPUT_FILE = 'tvs/CPU.tv'  # expects to be run from directory above this
+N = 10000
+i = 0
+
+cpusim = CPUSimulator()
+
+
+def gen_random_instruction() -> str:
+  '''
+  Function for generating a random instruction since there are some rules instructions should play by.
+  '''
+  possible_instruction = cpusim.int_to_bin_str(
+      randint(-32768, 32767), cpusim.WIDTH)
+  if possible_instruction[0] == '0':
+    # if this is an A instruction go ahead and return it right away
+    return possible_instruction
+  else:
+    # else this is a C instruction, possible_instruction[1:3] == '1'
+    # per the specification 4.2.3 The C-Instruction
+    possible_instruction = possible_instruction[
+        0] + '11' + possible_instruction[3:]
+    # a[4:10] must be a valid function for the alu
+    possible_instruction = possible_instruction[0:4] + \
+      ALUSimulator.funcs[randint(0, len(ALUSimulator.funcs) - 1)] + \
+      possible_instruction[10:]
+    return possible_instruction
+
+
+with open(OUTPUT_FILE, 'w') as f:
+  while i < N:
+    inM = randint(-32768, 32767)
+    instruction = gen_random_instruction()
+    reset = bool(randint(0, 1))
+    try:
+      f.write(cpusim.build_line(inM, instruction, reset))
+      i += 1
+    except UnkownALUFunction as e:
+      continue
