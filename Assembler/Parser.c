8dbb9f84b1c5ff19aafe351dd4a18da6878bf7a4
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-01-16 15:38:13 -0800

    Forgot how painful it is to work with strings in C. But its slowly coming along

---
+++ Assembler/Parser.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct parser_s {
+  FILE *input;
+  const char *input_filename;
+  FILE *output;
+  const char *output_filename;
+} parser_t;
+
+// Takes X.asm and returns X.hack
+const char *dot_hack_from_dot_asm(const char *dot_asm) {
+  size_t asm_num_chars = strlen(dot_asm);
+
+  // extra char for asm -> hack, plus null term
+  char *dot_hack = (char *)calloc(asm_num_chars + 2, sizeof(char));
+  // copy "X."
+  strncpy(dot_hack, dot_asm, asm_num_chars - 3);
+  // then make "X.hack"
+  strncpy(dot_hack + (asm_num_chars - 3), "hack", 4);
+
+  return dot_hack;
+}
+
+parser_t *Parser__create(const char *input_filename) {
+  parser_t *parser = (parser_t *)malloc(sizeof(parser_t));
+  parser->input_filename = input_filename;
+  parser->output_filename = dot_hack_from_dot_asm(input_filename);
+  return parser;
+}
+
+void Parser__destroy(parser_t *parser) {
+  // no need to free input_filename which comes from argv
+  free((void *)parser->output_filename);
+  free(parser);
+}
+
+int main(int argc, char *argv[]) {
+  parser_t *parser = Parser__create(argv[1]);
+  printf("input_filename = %s\n", parser->input_filename);
+  printf("output_filename = %s\n", parser->output_filename);
+  Parser__destroy(parser);
+  return 0;
+}
