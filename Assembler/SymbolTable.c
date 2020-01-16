964b4fdc638ccc0f66dac42f9ec7ec00fd6716d7
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-01-16 11:55:42 -0800

    clang format

--- Assembler/SymbolTable.c
+++ Assembler/SymbolTable.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 
 // Structure for table, see https://troydhanson.github.io/uthash/userguide.html
-typedef struct symbol_table_entry_s
-{
+typedef struct symbol_table_entry_s {
   const char *symbol; // key
   uint16_t value;     // unsigned, 16-bit according to Hack spec
   UT_hash_handle hh;  // makes this structure hashable
 } symbol_table_entry_t;
 
-// Creates pointer to symbol_table_entry_t equal to NULL, which can be thought of
-// as the "root" object for the table. All other functions will take a pointer to this
-// pointer to manipulate the table. From the documentation:
-// "The reason it’s necessary to deal with a pointer to the hash pointer is simple:
-// the hash macros modify it (in other words, they modify the pointer itself not just what it points to)."
-symbol_table_entry_t *SymbolTable__create(void)
-{
+// Creates pointer to symbol_table_entry_t equal to NULL, which can be thought
+// of as the "root" object for the table. All other functions will take a
+// pointer to this pointer to manipulate the table. From the documentation: "The
+// reason it’s necessary to deal with a pointer to the hash pointer is simple:
+// the hash macros modify it (in other words, they modify the pointer itself not
+// just what it points to)."
+symbol_table_entry_t *SymbolTable__create(void) {
   symbol_table_entry_t *symbol_table = NULL; // important! initialize to NULL
   return symbol_table;
 }
 
 // Add a new symbol to the table
-void SymbolTable__addEntry(symbol_table_entry_t **symbol_table, const char *symbol, size_t symbol_length, uint16_t value)
-{
+void SymbolTable__addEntry(symbol_table_entry_t **symbol_table,
+                           const char *symbol, size_t symbol_length,
+                           uint16_t value) {
   symbol_table_entry_t *new_entry;
   new_entry = malloc(sizeof(symbol_table_entry_t));
 
@@ -31,20 +31,20 @@ void SymbolTable__addEntry(symbol_table_entry_t **symbol_table, const char *symb
   new_entry->symbol = calloc(symbol_length + 1, sizeof(char));
   strncpy((char *)new_entry->symbol, symbol, symbol_length);
   new_entry->value = value;
-  HASH_ADD_KEYPTR(hh, *symbol_table, new_entry->symbol, symbol_length, new_entry);
+  HASH_ADD_KEYPTR(hh, *symbol_table, new_entry->symbol, symbol_length,
+                  new_entry);
 }
 
 // Checks if table contains symbol
-bool SymbolTable__contains(symbol_table_entry_t **symbol_table, char *symbol)
-{
+bool SymbolTable__contains(symbol_table_entry_t **symbol_table, char *symbol) {
   symbol_table_entry_t *s;
   HASH_FIND_STR(*symbol_table, symbol, s);
   return s != NULL;
 }
 
 // Gets the address of a given symbol
-uint16_t SymbolTable__getAddress(symbol_table_entry_t **symbol_table, char *symbol)
-{
+uint16_t SymbolTable__getAddress(symbol_table_entry_t **symbol_table,
+                                 char *symbol) {
   symbol_table_entry_t *s;
   HASH_FIND_STR(*symbol_table, symbol, s);
   // WARN: will segfault if symbol DNE
@@ -52,12 +52,10 @@ uint16_t SymbolTable__getAddress(symbol_table_entry_t **symbol_table, char *symb
 }
 
 // Deletes the table and cleans up all the memory
-void SymbolTable__destroy(symbol_table_entry_t **symbol_table)
-{
+void SymbolTable__destroy(symbol_table_entry_t **symbol_table) {
   symbol_table_entry_t *entry, *tmp;
 
-  HASH_ITER(hh, *symbol_table, entry, tmp)
-  {
+  HASH_ITER(hh, *symbol_table, entry, tmp) {
     HASH_DEL(*symbol_table, entry); // delete; symbol_table advances to next
     free((char *)entry->symbol);
     free(entry);
@@ -65,8 +63,7 @@ void SymbolTable__destroy(symbol_table_entry_t **symbol_table)
 }
 
 // TODO: Turn this into a test
-int main()
-{
+int main() {
   symbol_table_entry_t *symbol_table = SymbolTable__create();
   char *test1 = "test1";
   char *test2 = "test2";
@@ -76,7 +73,8 @@ int main()
     printf("Contains test1\n");
   if (!(SymbolTable__contains(&symbol_table, test2)))
     printf("Does not contain test2\n");
-  printf("SymbolTable has address (%d) for symbol %s\n", SymbolTable__getAddress(&symbol_table, test1), test1);
+  printf("SymbolTable has address (%d) for symbol %s\n",
+         SymbolTable__getAddress(&symbol_table, test1), test1);
   SymbolTable__destroy(&symbol_table);
   return 0;
 }
