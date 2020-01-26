acdb9eb40b7254898d130bc8a63aac327b8b0399
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-01-25 17:55:31 -0800

    beginning of the skeleton of the actual parser (will not compile)

--- Assembler/Parser.c
+++ Assembler/Parser.c
@@ -1,9 +1,17 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define BUF_SIZE 1024
+
+typedef enum {
+  INIT,
+  SKIP,
+  A_COMMAND,
+  C_COMMAND,
+  L_COMMAND,
+  SYNTAX_ERROR
+} line_type;
 
 typedef struct parser_s {
   FILE *input;
@@ -11,6 +19,7 @@ typedef struct parser_s {
   FILE *output;
   const char *output_filename;
   char current_line_buf[BUF_SIZE];
+  line_type current_line_type;
 } parser_t;
 
 // Takes X.asm and returns X.hack
@@ -29,13 +38,12 @@ const char *dot_hack_from_dot_asm(const char *dot_asm) {
 
 void remove_spaces(char *str_trimmed, char *str_untrimmed) {
   while (*str_untrimmed != '\0') {
-    if (!isspace(*str_untrimmed)) {
+    if (!(*str_untrimmed == ' ')) {
       *str_trimmed = *str_untrimmed;
       str_trimmed++;
     }
     str_untrimmed++;
   }
-  *str_trimmed++ = '\n'; // replace, isspace returns true for '\n'
   *str_trimmed = '\0';
 }
 
@@ -47,15 +55,44 @@ parser_t *Parser__create(const char *input_filename) {
   // Open input and output files
   parser->input = fopen(parser->input_filename, "r");
   parser->output = fopen(parser->output_filename, "w");
+  parser->current_line_type = INIT;
 
   return parser;
+}
+
+void set_command_type(parser_t *parser) {
+  char first_char = *(parser->current_line_buf);
+  char second_char = *(parser->current_line_buf + 1);
+
+  if (first_char == '\n') { // \n or // -> skip line
+    parser->current_line_type = SKIP;
+  } else if (first_char == '/') {
+    if (second_char == '/') {
+      parser->current_line_type = SKIP;
+    } else {
+      parser->current_line_type = SYNTAX_ERROR;
+    }
+  } else if (first_char == '@') {
+    // TODO: check that subsequent chars are valid
+    parser->current_line_type = A_COMMAND;
+  } else if (first_char == '(') {
+    // TODO: check that subsequent chars are valid
+    parser->current_line_type = L_COMMAND;
+  } else if (/* condition */) {
+    // D, A, M, 0, 1, -, !
+  }
 }
 
 void Parser__advance(parser_t *parser) {
   // Reads the next line into the parser->current_line_buf
   fgets(parser->current_line_buf, BUF_SIZE, parser->input);
-  // Yes, parser->current_line_buf for both args will remove spaces in place
+  // parser->current_line_buf for both args will remove spaces in place
   remove_spaces(parser->current_line_buf, parser->current_line_buf);
+  // \n or // -> skip line
+  // @ -> A_COMMAND
+  // ( -> L_COMMAND
+  // D, A, M, 0, 1, -, !
+  set_command_type(parser);
 }
 
 void Parser__destroy(parser_t *parser) {
