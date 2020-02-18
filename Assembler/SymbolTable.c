db5ac57a6cbdd938ff572a7a0736aba5533dbc74
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-02-17 22:58:41 -0800

    created header file for symboltable and simplified adding symbol

--- Assembler/SymbolTable.c
+++ Assembler/SymbolTable.c
@@ -1,31 +1,33 @@
+#include "SymbolTable.h"
 #include "uthash.h"
 #include <stdbool.h>
 #include <stdio.h>
 
-// Structure for table, see https://troydhanson.github.io/uthash/userguide.html
-typedef struct symbol_table_entry_s {
-  const char *symbol; // key
-  uint16_t value;     // unsigned, 16-bit according to Hack spec
-  UT_hash_handle hh;  // makes this structure hashable
-} symbol_table_entry_t;
-
-// Creates pointer to symbol_table_entry_t equal to NULL, which can be thought
-// of as the "root" object for the table. All other functions will take a
-// pointer to this pointer to manipulate the table. From the documentation: "The
-// reason it’s necessary to deal with a pointer to the hash pointer is simple:
-// the hash macros modify it (in other words, they modify the pointer itself not
-// just what it points to)."
 symbol_table_entry_t *SymbolTable__create(void) {
   symbol_table_entry_t *symbol_table = NULL; // important! initialize to NULL
+
+  // Initialize with predefined symbols acc. to spec in section 6.2.3
+  SymbolTable__addEntry(&symbol_table, "SP", 0);
+  SymbolTable__addEntry(&symbol_table, "LCL", 1);
+  SymbolTable__addEntry(&symbol_table, "ARG", 2);
+  SymbolTable__addEntry(&symbol_table, "THIS", 3);
+  SymbolTable__addEntry(&symbol_table, "THAT", 4);
+  SymbolTable__addEntry(&symbol_table, "SCREEN", 0x4000);
+  SymbolTable__addEntry(&symbol_table, "KDB", 0x6000);
+  char r[4];
+  for (int i = 0; i < 16; i++) {
+    sprintf(r, "R%d", i);
+    SymbolTable__addEntry(&symbol_table, r, i);
+  }
   return symbol_table;
 }
 
-// Add a new symbol to the table
 void SymbolTable__addEntry(symbol_table_entry_t **symbol_table,
-                           const char *symbol, size_t symbol_length,
-                           uint16_t value) {
+                           const char *symbol, uint16_t value) {
   symbol_table_entry_t *new_entry;
   new_entry = malloc(sizeof(symbol_table_entry_t));
+
+  size_t symbol_length = strlen(symbol);
 
   // allocate memory for chars in symbol plus the null terminator
   new_entry->symbol = calloc(symbol_length + 1, sizeof(char));
@@ -35,14 +37,12 @@ void SymbolTable__addEntry(symbol_table_entry_t **symbol_table,
                   new_entry);
 }
 
-// Checks if table contains symbol
 bool SymbolTable__contains(symbol_table_entry_t **symbol_table, char *symbol) {
   symbol_table_entry_t *s;
   HASH_FIND_STR(*symbol_table, symbol, s);
   return s != NULL;
 }
 
-// Gets the address of a given symbol
 uint16_t SymbolTable__getAddress(symbol_table_entry_t **symbol_table,
                                  char *symbol) {
   symbol_table_entry_t *s;
@@ -51,7 +51,6 @@ uint16_t SymbolTable__getAddress(symbol_table_entry_t **symbol_table,
   return s->value;
 }
 
-// Deletes the table and cleans up all the memory
 void SymbolTable__destroy(symbol_table_entry_t **symbol_table) {
   symbol_table_entry_t *entry, *tmp;
 
@@ -68,7 +67,7 @@ int main() {
   char *test1 = "test1";
   char *test2 = "test2";
   uint16_t val = 5;
-  SymbolTable__addEntry(&symbol_table, test1, 5, val);
+  SymbolTable__addEntry(&symbol_table, test1, val);
   if (SymbolTable__contains(&symbol_table, "test1"))
     printf("Contains test1\n");
   if (!(SymbolTable__contains(&symbol_table, test2)))
---
+++ Assembler/SymbolTable.h
@@ -0,0 +1,32 @@
+#include "uthash.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+// Structure for table, see https://troydhanson.github.io/uthash/userguide.html
+typedef struct symbol_table_entry_s {
+  const char *symbol; // key
+  uint16_t value;     // unsigned, 16-bit according to Hack spec
+  UT_hash_handle hh;  // makes this structure hashable
+} symbol_table_entry_t;
+
+// Creates pointer to symbol_table_entry_t equal to NULL, which can be thought
+// of as the "root" object for the table. All other functions will take a
+// POINTER to this pointer to manipulate the table. From the documentation: "The
+// reason it’s necessary to deal with a pointer to the hash pointer is simple:
+// the hash macros modify it (in other words, they modify the pointer itself not
+// just what it points to)."
+symbol_table_entry_t *SymbolTable__create(void);
+
+// Add a new symbol to the table
+void SymbolTable__addEntry(symbol_table_entry_t **symbol_table,
+                           const char *symbol, uint16_t value);
+
+// Checks if table contains symbol
+bool SymbolTable__contains(symbol_table_entry_t **symbol_table, char *symbol);
+
+// Gets the address of a given symbol
+uint16_t SymbolTable__getAddress(symbol_table_entry_t **symbol_table,
+                                 char *symbol);
+
+// Deletes the table and cleans up all the memory
+void SymbolTable__destroy(symbol_table_entry_t **symbol_table);
