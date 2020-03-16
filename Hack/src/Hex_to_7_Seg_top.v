32c30dab8e5cde45693c815d0b9db7968f4328c7
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-03-15 18:45:59 -0700

    adding logic for using all 4 of the seven seg digits

--- Hack/src/Hex_to_7_Seg_top.v
+++ Hack/src/Hex_to_7_Seg_top.v
@@ -3,6 +3,8 @@ module Hex_to_7_Seg_top (
 	input reset,
 	input [3:0] hex_in_0,
 	input [3:0] hex_in_1,
+	input [3:0] hex_in_2,
+	input [3:0] hex_in_3,
 	output reg [6:0] seg_out,
 	output enable0,
 	output enable1,	
@@ -11,11 +13,13 @@ module Hex_to_7_Seg_top (
 
 wire [6:0] seg_out_0;
 wire [6:0] seg_out_1;
-reg [1:0] toggle = 2'b10;
-assign enable0 = toggle[0];
-assign enable1 = toggle[1];
-assign enable2 = 1'b1;
-assign enable3 = 1'b1;
+wire [6:0] seg_out_2;
+wire [6:0] seg_out_3;
+reg [1:0] toggle = 2'b00;
+reg enable0;
+reg enable1;
+reg enable2;
+reg enable3;
 
 reg [19:0] refresh_counter; 
 
@@ -29,6 +33,16 @@ Hex_to_7_Seg Hex_to_7_Seg_1 (
 	.seven_seg(seg_out_1)
 );
 
+Hex_to_7_Seg Hex_to_7_Seg_2 (
+	.hex(hex_in_2),
+	.seven_seg(seg_out_2)
+);
+
+Hex_to_7_Seg Hex_to_7_Seg_3 (
+	.hex(hex_in_3),
+	.seven_seg(seg_out_3)
+);
+
 always @(posedge clk)
 begin 
  if(reset)
@@ -40,15 +54,42 @@ end
 always @(posedge clk)
 begin
 	if (refresh_counter == {20{1'b1}})
-		toggle = ~toggle;
+		toggle <= toggle + 1;
 end
 
 always @(toggle, seg_out_0, seg_out_1)
 begin
-	if (toggle[1] == 1'b1)
-		seg_out = seg_out_1;
-	else
-		seg_out = seg_out_0;
+	case (toggle)
+		2'b00: begin
+			seg_out = seg_out_0;
+			enable0 = 1'b0;
+			enable1 = 1'b1;
+			enable2 = 1'b1;
+			enable3 = 1'b1;
+		end
+		2'b01: begin
+			seg_out = seg_out_1;
+			enable0 = 1'b0;
+			enable1 = 1'b1;
+			enable2 = 1'b1;
+			enable3 = 1'b1;
+		end
+		2'b10: begin
+			seg_out = seg_out_2;
+			enable0 = 1'b0;
+			enable1 = 1'b1;
+			enable2 = 1'b1;
+			enable3 = 1'b1;
+		end
+		2'b11: begin
+			seg_out = seg_out_3;
+			enable0 = 1'b0;
+			enable1 = 1'b1;
+			enable2 = 1'b1;
+			enable3 = 1'b1;
+		end
+		default: 
+	endcase
 end
 
 endmodule // Hex_to_7_Seg_top
