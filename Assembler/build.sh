ff53e9eaf93f2943c12551dc2aee3314c243aed3
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-08-16 19:57:36 -0600

    adding bash code for selecting between gcc and clang, adding .hack test outputs to the gitignore

--- Assembler/build.sh
+++ Assembler/build.sh
@@ -1,3 +1,13 @@
 set -e
 
-clang -o Assembler main.c Parser.c SymbolTable.c -I./
+COMPILER=""
+if command -v clang &> /dev/null; then
+    COMPILER=clang
+elif command -v gcc &> /dev/null; then 
+    COMPILER=gcc
+else 
+    printf "This program requires clang or gcc to be installed\n" 1>&2
+    exit 1
+fi 
+
+"$COMPILER" -o Assembler main.c Parser.c SymbolTable.c -I./
--- Assembler/build_tests.sh
+++ Assembler/build_tests.sh
@@ -1,4 +1,14 @@
 set -e
 
-clang -o Test/Parser__advance_test Parser.c SymbolTable.c Test/Parser__advance_test.c -I./ -D TEST
-clang -o Test/SymbolTable_test SymbolTable.c Test/SymbolTable_test.c -I./ -D TEST
+COMPILER=""
+if command -v clang &> /dev/null; then
+    COMPILER=clang
+elif command -v gcc &> /dev/null; then 
+    COMPILER=gcc
+else 
+    printf "This program requires clang or gcc to be installed\n" 1>&2
+    exit 1
+fi 
+
+"$COMPILER" -o Test/Parser__advance_test Parser.c SymbolTable.c Test/Parser__advance_test.c -I./ -D TEST
+"$COMPILER" -o Test/SymbolTable_test SymbolTable.c Test/SymbolTable_test.c -I./ -D TEST
