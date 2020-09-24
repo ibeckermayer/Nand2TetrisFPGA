bcfa81f0157bca87af4df978b131060075fab1e7
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-09-23 18:33:45 -0600

    reverting back to unclocked ROM because for some reason that triggers some optimization that I can't track down, and ROM dissapears

--- Hack/src/ROM32K.v
+++ Hack/src/ROM32K.v
@@ -1,9 +1,6 @@
 module ROM32K #(parameter inputfile = "") // binary program to load into memory
-       (
-           input clk,
-           input [15:0] address,
-           output reg [15:0] out
-        );
+       (input [15:0] address,
+        output [15:0] out);
 
 reg [15:0]    rom [32767: 0]; // 32k ram at 16-bit address width
 
@@ -12,9 +9,6 @@ begin
     $readmemb(inputfile, rom);
 end
 
-always @(posedge clk)
-begin
-    out <= rom[address];
-end
+assign out = rom[address];
 
 endmodule // ROM32K
