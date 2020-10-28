014cc2f119e508034a5938c4f4ef960696ddfd9e
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-10-27 17:08:03 -0700

    PointerTest and StaticTest both added and passing without hiccups

--- VMtranslator/test/memory_access_test.py
+++ VMtranslator/test/memory_access_test.py
@@ -24,3 +24,36 @@ def test_BasicTest():
     assert hack.ram[256] == 472
     assert hack.ram[3000] == 8
     assert hack.ram[3010] == 7
+
+
+def test_PointerTest():
+    # Run the VMtranslator
+    VMtranslator('test/PointerTest.vm').run()
+    # Load the resulting asm file into the HackExecutor
+    hack = HackExecutor(AsmParser('test/PointerTest.asm').run())
+    # Simulate program to the end
+    while True:
+        if hack.step().type == CT.END:
+            break
+
+    assert hack.ram[256] == 6084
+    assert hack.ram[3] == 3030
+    assert hack.ram[4] == 3040
+    assert hack.ram[3032] == 32
+    assert hack.ram[3046] == 46
+
+
+def test_StaticTest():
+    # Run the VMtranslator
+    VMtranslator('test/StaticTest.vm').run()
+    # Load the resulting asm file into the HackExecutor
+    hack = HackExecutor(AsmParser('test/StaticTest.asm').run())
+    # Simulate program to the end
+    while True:
+        if hack.step().type == CT.END:
+            break
+
+    assert hack.ram[256] == 1110
+    assert hack.ram[24] == 888
+    assert hack.ram[19] == 333
+    assert hack.ram[17] == 111
