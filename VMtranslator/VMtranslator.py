b1188e33fe26d770006371aec0e1c5c8f30f1993
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-11-18 22:03:49 -0800

    first attempt with function calls

--- VMtranslator/VMtranslator.py
+++ VMtranslator/VMtranslator.py
@@ -11,9 +11,9 @@ class CommandType(Enum):
     LABEL = 4
     GOTO = 5
     IF_GOTO = 6
-    # C_FUNCTION = 7
-    # C_RETURN = 8
-    # C_CALL = 9
+    FUNCTION = 7
+    RETURN = 8
+    CALL = 9
 
 
 # Give CommandType an alias to make it a bit easier on the eye
@@ -35,6 +35,7 @@ class Parser:
         self.lines: TextIO = open(self.filename, 'r')
         self.cur_line: str = ''  # The current line being read, as a string
         self.cur_line_split: List[str] = []  # cur_line tokenized into a list
+        self.cur_line_number = 0
         self.command_type: Optional[
             CommandType] = None  # The type of command cur_line is interpreted as
 
@@ -72,9 +73,16 @@ class Parser:
                 self.command_type = CT.IF_GOTO
             elif (self.cur_line_split[0] == 'goto'):
                 self.command_type = CT.GOTO
+            elif (self.cur_line_split[0] == 'function'):
+                self.command_type = CT.FUNCTION
+            elif (self.cur_line_split[0] == 'return'):
+                self.command_type = CT.RETURN
+            elif (self.cur_line_split[0] == 'call'):
+                self.command_type = CT.CALL
 
         # Read the next line and strip leading/trailing spaces
         self.cur_line = self.lines.readline().strip(' ')
+        self.cur_line_number += 1
 
         # If EOF, return EOF
         if self.cur_line == '':
@@ -107,7 +115,8 @@ class CodeWriter:
 
     def __init__(self, output_filename: str):
         self.output_file = open(output_filename, 'w')
-        self.current_parser_filename = ''  # Gets updated per parser, used for naming static variables
+        self.cur_parser_filename = ''  # Gets updated per parser, used for naming static variables
+        self.cur_func_name = ''  # Gets updated per function declaration, used for naming labels within functions
         self.eq_num = 0  # Used as an identifier in `eq` operations
         self.gt_num = 0  # Used as an identifier in `gt` operations
         self.lt_num = 0  # Used as an identifier in `lt` operations
@@ -130,7 +139,41 @@ class CodeWriter:
         Xxx.vm contains the command push static 3. This command can be translated to the Hack assembly commands @Xxx.3 and D=M, followed 
         by additional assembly code that pushes D’s value to the stack. This implementation of the static segment is somewhat tricky, but it works."
         '''
-        return self.current_parser_filename.split('/')[-1].split('.')[0] + f".{suffix}"
+        return self.cur_parser_filename.split('/')[-1].split('.')[0] + f".{suffix}"
+
+    def prefix_w_cur_func_name(self, label: str) -> str:
+        '''
+        Prefixes label with f"{self.cur_func_name}$". From the book (Figure 8.6):
+        "Each `label b` command in a VM function `f` should generate a globally uniques symbol `f$b`..."
+
+        If self.cur_func_name isn't set (empty string) then just return label (this is kinda jank but expedient for testing)
+        '''
+        if self.cur_func_name == '':
+            return label
+        return f"{self.cur_func_name}${label}"
+
+    def create_ret_addr(self, cur_func_name: str, cur_line_number: int) -> str:
+        '''
+        Creates a unique return-address symbol for implementing the `call f n` stack machine function call operation.
+        Ensures that the symbol is unique by using the function name and the (letter encoded) line number of the calling function.
+        '''
+
+        def int_to_ascii(input: int) -> str:
+            '''
+            Converts any positive integer value to a string, to be used on for the return address.
+            This is necessary due to the Assembler spec: "Constants must be non-negative and are written in decimal notation. 
+            A user-defined symbol can be any sequence of letters, digits, underscore (_), dot (.), dollar sign ($), and colon (:) 
+            that does not begin with a digit."
+            '''
+            ret_val = ''
+            a = input
+            while a != 0:
+                ret_val += chr(97 + (a % 10))
+                a = int(a / 10)
+            return ret_val
+
+        # 'ra' short for "return address"
+        return f"ra_{cur_func_name}_{int_to_ascii(cur_line_number)}"
 
     def set_reg(self, symbol: str, value: int):
         '''
@@ -170,7 +213,7 @@ class CodeWriter:
         i.e. if we encounter `push local 2` we call `self.push_value("LCL", 2)`
         
         In this example, we will find the base pointer stored in the LCL register, add 2 to it to create our final pointer, 
-        then push the value pointedto by that pointer onto the stack
+        then push the value pointed to by that pointer onto the stack
         '''
         # Build the pointer in the A register
         self.output_file.write(f"@{symbol}\n")
@@ -258,6 +301,20 @@ class CodeWriter:
         '''
         self.output_file.write(f"@SP\n")
         self.output_file.write(f"A=M\n")
+
+    def load_SP_into_D(self):
+        '''
+        Loads the value of SP into the D register
+        '''
+        self.output_file.write(f"@SP\n")
+        self.output_file.write(f"D=M\n")
+
+    def goto_label(self, label: str):
+        '''
+        Loads address `label` into the A register and then jumps to that instruction
+        '''
+        self.output_file.write(f"@{label}\n")
+        self.output_file.write(f"0;JMP\n")
 
     def write_arithmetic(self, parser: Parser):
         '''
@@ -472,18 +529,22 @@ class CodeWriter:
 
         self.output_file.write(f'\n')
 
+    def push_constant(self, val: str):
+        '''
+        Pushes val onto the stack and incrememnts the stack pointer
+        '''
+        self.output_file.write(f"@{val}\n")
+        self.output_file.write(f"D=A\n")
+        self.load_SP_into_A()
+        self.output_file.write(f"M=D\n")
+        self.SP_pp(load_SP_into_A=False)
+
     def write_push(self, parser: Parser):
         # Write a comment with the VM code for reference/debugging
         self.output_file.write(f"// {' '.join(parser.cur_line_split[:3])}\n")
 
         if parser.cur_line_split[1] == "constant":
-            val: str = parser.cur_line_split[2]
-
-            self.output_file.write(f"@{val}\n")
-            self.output_file.write(f"D=A\n")
-            self.load_SP_into_A()
-            self.output_file.write(f"M=D\n")
-            self.SP_pp(load_SP_into_A=False)
+            self.push_constant(parser.cur_line_split[2])
         elif parser.cur_line_split[1] == "local":
             self.push_value("LCL", int(parser.cur_line_split[2]))
         elif parser.cur_line_split[1] == "argument":
@@ -537,7 +598,7 @@ class CodeWriter:
         pass
 
     def write_label(self, parser: Parser):
-        self.output_file.write(f"({self.static_symbol(parser.cur_line_split[1])})\n")
+        self.output_file.write(f"({self.prefix_w_cur_func_name(parser.cur_line_split[1])})\n")
         self.output_file.write(f"\n")
 
     def write_if_goto(self, parser: Parser):
@@ -545,7 +606,7 @@ class CodeWriter:
         This command effects a conditional goto operation. The stack’s topmost value is popped; 
         if the value is not zero, execution continues from the location marked by the label; otherwise, 
         execution continues from the next command in the program. 
-        TODO: The jump destination must be located in the same function.
+        The jump destination must be located in the same function.
         '''
         self.output_file.write(f"// {' '.join(parser.cur_line_split[:2])}\n")
         # Pop the top of the stack into the D register
@@ -553,7 +614,7 @@ class CodeWriter:
         self.output_file.write(f"D=M\n")
 
         # Load the label into the A register
-        self.output_file.write(f"@{self.static_symbol(parser.cur_line_split[1])}\n")
+        self.output_file.write(f"@{self.prefix_w_cur_func_name(parser.cur_line_split[1])}\n")
 
         # If popped value is non-zero, jump to label
         self.output_file.write(f"D;JNE\n")
@@ -563,14 +624,139 @@ class CodeWriter:
     def write_goto(self, parser: Parser):
         '''
         This command effects an unconditional goto operation, causing execution to continue from the location marked by the label. 
-        TODO: The jump destination must be located in the same function.
+        The jump destination must be located in the same function.
         '''
         self.output_file.write(f"// {' '.join(parser.cur_line_split[:2])}\n")
 
-        # Load the label into the A register
-        self.output_file.write(f"@{self.static_symbol(parser.cur_line_split[1])}\n")
+        self.goto_label(self.prefix_w_cur_func_name(parser.cur_line_split[1]))
 
-        # Jump to that label
+        self.output_file.write(f"\n")
+
+    def write_function(self, parser: Parser):
+        '''
+        `function f k`: declaring a function `f` that has `k` local variables
+        ```psuedocode
+        (f)
+        repeat k times:
+        PUSH 0
+        ```
+        See Figure 8.5 on p. 193
+        '''
+        self.output_file.write(f"// {' '.join(parser.cur_line_split[:2])}\n")
+
+        f = parser.cur_line_split[1]
+        k = int(parser.cur_line_split[2])
+
+        # Set cur_func_name so that labels within this function can be written as `functionName$label`
+        self.cur_func_name = f
+
+        # Create label for the function itself
+        self.output_file.write(f"({f})\n")
+
+        # Push k local variables onto the stack, initialized to 0
+        for _ in range(k):
+            self.push_constant("0")
+
+    def write_call(self, parser: Parser):
+        '''
+        `call f n`: calling a function `f` after `n` arguments have been pushed onto the stack
+        ```psuedocode
+        push return-address
+        push LCL
+        push ARG
+        push THIS
+        push THAT
+        ARG = SP - n - 5
+        LCL = SP
+        goto f
+        (return-address)
+        ```
+        See Figure 8.5 on p. 193
+        '''
+        self.output_file.write(f"// {' '.join(parser.cur_line_split[:2])}\n")
+
+        f = parser.cur_line_split[1]
+        n = parser.cur_line_split[2]
+        ret_addr = self.create_ret_addr(self.cur_func_name, parser.cur_line_number)
+
+        self.push_constant(ret_addr)
+        self.push_pointer("LCL")
+        self.push_pointer("ARG")
+        self.push_pointer("THIS")
+        self.push_pointer("THAT")
+
+        # Load SP into the D register
+        self.load_SP_into_D()
+
+        # LCL = SP
+        self.output_file.write(f"@LCL\n")
+        self.output_file.write(f"M=D\n")
+
+        # ARG = SP - n - 5
+        self.output_file.write(f"@{n}\n")
+        self.output_file.write(f"D=D-A\n")
+        self.output_file.write(f"@{5}\n")
+        self.output_file.write(f"D=D-A\n")
+        self.output_file.write(f"@ARG\n")
+        self.output_file.write(f"M=D\n")
+
+        self.goto_label(f)
+        self.output_file.write(f"({ret_addr})\n")
+        self.output_file.write(f"\n")
+
+    def write_return(self, parser: Parser):
+        '''
+        ```psuedocode
+        FRAME = LCL
+        RET = *(FRAME - 5)
+        *ARG = pop()
+        SP = ARG+1
+        THAT = *(FRAME - 1)
+        THIS = *(FRAME - 2)
+        ARG = *(FRAME - 3)
+        LCL = *(FRAME - 4)
+        goto RET
+        ```
+        See Figure 8.5 on p. 193
+        '''
+        self.output_file.write(f"// {parser.cur_line_split[0]}\n")
+        # FRAME = LCL
+        # RET = *(FRAME - 5)
+        self.output_file.write(f"@LCL\n")
+        self.output_file.write(f"D=M\n")
+        self.output_file.write(f"@5\n")
+        self.output_file.write(f"D=D-A\n")
+        # Save in register 13
+        self.output_file.write(f"@R13\n")
+        self.output_file.write(f"M=D\n")
+
+        # *ARG = pop()
+        # reposition the return value from the current top of stack to the caller's top of stack (which is the current ARG)
+        self.pop_value("ARG", 0)
+
+        # SP = ARG+1
+        self.output_file.write(f"@ARG\n")
+        self.output_file.write(f"D=M+1\n")
+        self.output_file.write(f"@SP\n")
+        self.output_file.write(f"M=D\n")
+
+        # THAT = *(FRAME - 1)
+        # THIS = *(FRAME - 2)
+        # ARG = *(FRAME - 3)
+        # LCL = *(FRAME - 4)
+        for destination in ['THAT', 'THIS', 'ARG', 'LCL']:
+            self.output_file.write(f"@LCL\n")
+            # LCL = FRAME-1 so that the loop works, A = FRAME-1
+            self.output_file.write(f"AM=M-1\n")
+            # D = *(FRAME-1), the saved value
+            self.output_file.write(f"D=M\n")
+            # destination = D
+            self.output_file.write(f"@{destination}\n")
+            self.output_file.write(f"M=D\n")
+
+        # goto RET
+        self.output_file.write(f"@R13\n")
+        self.output_file.write(f"A=M\n")
         self.output_file.write(f"0;JMP\n")
 
         self.output_file.write(f"\n")
@@ -595,8 +781,8 @@ class VMtranslator:
         else:
             # Else directory_or_filename is a directory, walk through each file and give it a parser
             for filename in os.listdir(directory_or_filename):
-                if directory_or_filename.split('.')[-1] == 'vm':
-                    self.parsers.append(Parser(filename))
+                if filename.split('.')[-1] == 'vm':
+                    self.parsers.append(Parser(f"{directory_or_filename}/{filename}"))
 
         # Create CodeWriter
         self.codewriter = CodeWriter(directory_or_filename.split('.')[0].strip('/') + '.asm')
@@ -607,8 +793,8 @@ class VMtranslator:
         writes the corresponding assembly code to the output file.
         '''
         for parser in self.parsers:
-            # Update current_parser_filename so codewriter knows how to name static vars (section 7.3 in the book)
-            self.codewriter.current_parser_filename = parser.filename
+            # Update cur_parser_filename so codewriter knows how to name static vars (section 7.3 in the book)
+            self.codewriter.cur_parser_filename = parser.filename
             while (parser.advance()):
                 # parser parses the input, codewriter translates tokens to assembly, this section
                 # contains the logic for which codewrite function to pass the parser to
@@ -626,6 +812,12 @@ class VMtranslator:
                     self.codewriter.write_if_goto(parser)
                 elif parser.command_type == CT.GOTO:
                     self.codewriter.write_goto(parser)
+                elif parser.command_type == CT.FUNCTION:
+                    self.codewriter.write_function(parser)
+                elif parser.command_type == CT.RETURN:
+                    self.codewriter.write_return(parser)
+                elif parser.command_type == CT.CALL:
+                    self.codewriter.write_call(parser)
 
         # Don't forget to close the output file when you're done
         self.codewriter.output_file.close()
